e53ca1e0ed6b9637276bfe18acb95c7dfb17d932
HS0204 <lhanseul0204@naver.com>
2023-06-29 15:52:56 +0900

    [FEAT] 카카오 api 기본 작업

--- app/src/main/java/com/hs/test/maptest/KakaoFragment.kt
+++ app/src/main/java/com/hs/test/maptest/KakaoFragment.kt
@@ -1,9 +1,19 @@
 package com.hs.test.maptest
 
 import com.hs.test.maptest.databinding.FragmentKakaoBinding
+import com.hs.test.maptest.helper.KakaoMapHelper
+import net.daum.mf.map.api.MapView
 
 class KakaoFragment : BaseFragment<FragmentKakaoBinding>(FragmentKakaoBinding::inflate) {
+
+    private lateinit var mapViewHelper: KakaoMapHelper
+    private lateinit var mapView: MapView
+
     override fun initView() {
+        mapViewHelper = KakaoMapHelper(requireContext())
+        mapView = mapViewHelper.mapView
+
+        binding.kakaoMapView.addView(mapView)
     }
 
 }
---
+++ app/src/main/java/com/hs/test/maptest/helper/KakaoMapHelper.kt
@@ -0,0 +1,12 @@
+package com.hs.test.maptest.helper
+
+import android.content.Context
+import net.daum.mf.map.api.MapView
+
+class KakaoMapHelper(
+    context: Context,
+) {
+
+    val mapView: MapView = MapView(context)
+
+}
