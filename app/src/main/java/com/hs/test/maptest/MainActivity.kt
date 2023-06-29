2dc07566ee6a5fd331e004ab231101fb59467a47
HS0204 <lhanseul0204@naver.com>
2023-06-29 15:52:43 +0900

    [FEAT] debug 해시, 위치 얻기

--- app/src/main/java/com/hs/test/maptest/MainActivity.kt
+++ app/src/main/java/com/hs/test/maptest/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.hs.test.maptest
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +23,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        reqPermission()
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNav.setupWithNavController(navController)
+    }
+
+    private fun getDebugKeyHash() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val packageInfo =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+            for (signature in packageInfo.signingInfo.apkContentsSigners) {
+                try {
+                    val md = MessageDigest.getInstance("SHA")
+                    md.update(signature.toByteArray())
+                    Log.d(
+                        "getKeyHash",
+                        "key hash: ${Base64.encodeToString(md.digest(), Base64.NO_WRAP)}"
+                    )
+                } catch (e: NoSuchAlgorithmException) {
+                    Log.w("getKeyHash", "Unable to get MessageDigest. signature=$signature", e)
+                }
+            }
+        }
+    }
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 101
+
+    private fun reqPermission() {
+        val fineLocationPermission = android.Manifest.permission.ACCESS_FINE_LOCATION
+        val coarseLocationPermission = android.Manifest.permission.ACCESS_COARSE_LOCATION
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, fineLocationPermission) ||
+            ActivityCompat.shouldShowRequestPermissionRationale(this, coarseLocationPermission)
+        ) {
+            Toast.makeText(this, "지도를 사용하기 위해 위치 권한이 필요합니다", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(fineLocationPermission, coarseLocationPermission),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
     }
 }
