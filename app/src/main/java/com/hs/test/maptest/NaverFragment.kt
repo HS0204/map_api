c13e4fa71bced7d2e23056950ac29aea8ef1a272
HS0204 <lhanseul0204@naver.com>
2023-06-29 16:33:43 +0900

    [FEAT] 네이버 api 기본 작업

--- app/src/main/java/com/hs/test/maptest/NaverFragment.kt
+++ app/src/main/java/com/hs/test/maptest/NaverFragment.kt
@@ -1,9 +1,12 @@
 package com.hs.test.maptest
 
 import com.hs.test.maptest.databinding.FragmentNaverBinding
+import com.naver.maps.map.NaverMapSdk
 
 class NaverFragment : BaseFragment<FragmentNaverBinding>(FragmentNaverBinding::inflate) {
     override fun initView() {
+        NaverMapSdk.getInstance(requireContext()).client =
+            NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_NATIVE_APP_KEY)
     }
 
 }
